5483b98587a7b094148ff0934b19079bbf24a417
Newsstate <newsstate6294@gmail.com>
2025-08-21 18:32:02 +0530

    Update Card.tsx 2

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,18 +1,15 @@
+// components/card.tsx
 import * as React from "react";
 import { cn } from "@/lib/format";
 
-export type CardProps = React.ComponentProps<"div">;
+export type CardProps = React.ComponentPropsWithoutRef<"div">;
+
+const base =
+  "rounded-2xl bg-white dark:bg-slate-900 ring-1 ring-slate-200/60 dark:ring-white/10 shadow-sm";
 
 const Card = React.forwardRef<HTMLDivElement, CardProps>(
   ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        "rounded-2xl bg-white dark:bg-slate-900 ring-1 ring-slate-200/60 dark:ring-white/10 shadow-sm",
-        className
-      )}
-      {...props}  // <-- id, onClick, style, etc. now allowed
-    />
+    <div ref={ref} className={cn(base, className)} {...props} />
   )
 );
 
